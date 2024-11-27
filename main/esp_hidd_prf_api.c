dcdf4b2a9070d75f79d8f899627465dbb3af7d8c
FeatheryVlad <126783011+FeatheryVlad@users.noreply.github.com>
2024-11-27 22:32:53 +0300

    Code upload

---
+++ main/esp_hidd_prf_api.c
@@ -0,0 +1,59 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#include "esp_hidd_prf_api.h"
+#include "hidd_le_prf_int.h"
+#include "hid_dev.h"
+#include <stdlib.h>
+#include <string.h>
+#include "esp_log.h"
+
+// HID keyboard input report length
+#define HID_KEYBOARD_IN_RPT_LEN     8
+
+// HID LED output report length
+#define HID_LED_OUT_RPT_LEN         1
+
+// HID mouse input report length
+#define HID_MOUSE_IN_RPT_LEN        5
+
+// HID consumer control input report length
+#define HID_CC_IN_RPT_LEN           2
+
+esp_err_t esp_hidd_profile_init(void)
+{
+     if (hidd_le_env.enabled) {
+        ESP_LOGE(HID_LE_PRF_TAG, "HID device profile already initialized");
+        return ESP_FAIL;
+    }
+    // Reset the hid device target environment
+    memset(&hidd_le_env, 0, sizeof(hidd_le_env_t));
+    hidd_le_env.enabled = true;
+    return ESP_OK;
+}
+
+esp_err_t esp_hidd_register_callbacks(esp_hidd_event_cb_t callbacks)
+{
+    esp_err_t hidd_status;
+
+    if(callbacks != NULL) {
+   	    hidd_le_env.hidd_cb = callbacks;
+    } else {
+        return ESP_FAIL;
+    }
+
+    if((hidd_status = hidd_register_cb()) != ESP_OK) {
+        return hidd_status;
+    }
+
+    esp_ble_gatts_app_register(BATTRAY_APP_ID);
+
+    if((hidd_status = esp_ble_gatts_app_register(HIDD_APP_ID)) != ESP_OK) {
+        return hidd_status;
+    }
+
+    return hidd_status;
+}
